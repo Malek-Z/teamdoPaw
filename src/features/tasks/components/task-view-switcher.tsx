b08031c96c6608ec58fdfc4b0df9e70ad308cb80
Malek-Z <abdou.ziro1@gmail.com>
2024-12-08 09:42:26 +0100

    mlk

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -15,6 +15,7 @@ import useCreateTaskModal from "../hooks/use-create-task-modal";
 import { useTaskFilters } from "../hooks/use-task-filters";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
+import { DataKanban } from "./data-kanban";
 
 export const TaskViewSwitcher = () => {
 
@@ -57,10 +58,10 @@ export const TaskViewSwitcher = () => {
           ) : (
           <>
             <TabsContent value="table" className="mt'0">
-              <DataTable columns={columns} data={tasks?.documents ?? []}/>
+              <DataTable columns={ columns } data={ tasks?.documents ?? [] }/>
             </TabsContent>  
             <TabsContent value="kanban" className="mt-0">
-              {JSON.stringify(tasks)}
+              <DataKanban data={ tasks?.documents ?? [] }/>
             </TabsContent>   
             <TabsContent value="calendar" className="mt-0">
               {JSON.stringify(tasks)}
